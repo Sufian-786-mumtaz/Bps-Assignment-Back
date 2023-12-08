1968d9b1814594e04e623d763471cc91ebc1e305
haseeb-rehman-solutionsloft <haseeb.rehman@solutionsloft.com>
2023-12-08 14:25:59 +0500

    add netlify configurations

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 const app = express();
-const port = 8001;
-const url = 'http://2.233.121.120:1085/energy.php';
+// const port = 8001;
+const url = "http://2.233.121.120:1085/energy.php";
+const router = Router();
 app.use(cors());
-app.get('/energy', async(req, res)=>{
-    try {
-        const response = await axios.get(url);
-        res.send(response.data);
-      } catch (error) {
-        console.error('Error making API call:', error);
-        res.status(500).send('Internal Server Error');
-      }
-})
+router.get("/energy", async (req, res) => {
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error making API call:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+api.use("/api/", router);
 
-app.listen(port, ()=>{  
-    console.log(`server is running on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`server is running on port ${port}`);
+// });
+
+export const handler = serverless(app);
